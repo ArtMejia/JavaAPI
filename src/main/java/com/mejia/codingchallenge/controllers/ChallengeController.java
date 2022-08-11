8e85eef1a60c863e4977e11e66df4f15e0a75d6f
ArtMejia <arteniomejia.student@careerdevs.com>
2022-08-11 19:55:33 -0400

    update

--- src/main/java/com/mejia/codingchallenge/controllers/ChallengeController.java
+++ src/main/java/com/mejia/codingchallenge/controllers/ChallengeController.java
@@ -5,38 +5,38 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/data")
 public class ChallengeController {
 
     LocalDate date = LocalDate.now();
     LocalTime time = LocalTime.now();
 
-    @GetMapping("/data")
+    @GetMapping
     public String testGetRoute() {
         return "This is just a test "+ date + " " + time;
     }
 
-    @DeleteMapping("/data")
+    @DeleteMapping
     public String testDeleteRoute() {
         return "Delete here "+ date + " " + time;
     }
 
-    @PatchMapping("/data")
+    @PatchMapping
     public String testUpdateRoute() {
         return "Update Here " + date + " " + time;
     }
 
-    @PostMapping("/data")
+    @PostMapping
     public String testPostRoute() {
         return "Post here " + date + " " + time;
     }
 
-    @GetMapping("/data/{num}/{num2}")
+    @GetMapping("/{num}/{num2}")
     public String testRouteWithPathVar(@PathVariable int num, @PathVariable int num2) {
         return "Multiply " + num + " by " + num2 + " equals " + (num * num2) + " " + date + " " + time;
     }
 
-    @GetMapping("/data/")
+    @GetMapping("/")
     @ResponseBody
     public String testRouteWithReqParam(@RequestParam String id, @RequestParam(name = "blank") String name, @RequestParam String about) {
         return "Name: " + name + "\nID: " + id + "\nBio: " + about;
